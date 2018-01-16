266960faf2aaf12d83fd72b2488a89c75d26e037
senzitgit <34739394+senzitgit@users.noreply.github.com>
2018-01-16 22:30:50 +0530

    Version 2.4
    IBM APIs Added

---
+++ WebContent/js/Controllers/webServerService.js
@@ -0,0 +1,9 @@
+var webServerUrl = "https://lande.mybluemix.net/AppServerService.jsp";
+var changePicUrl = "https://lande.mybluemix.net/ChangePictureService.jsp";
+
+/*var webServerUrl = "http://localhost:8081/LawAndEnforcement/AppServerService.jsp";
+var changePicUrl = "http://localhost:8081/LawAndEnforcement/ChangePictureService.jsp";*/
+
+
+DownloadUrl = "C://apache-tomcat-7.0.54//webapps//Rebbon//CaseRecords";
+
